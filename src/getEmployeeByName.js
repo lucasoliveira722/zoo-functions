75932a0cc60a9bb7d4083fa1c23c0e5fbb74fce0
Lucas Oliveira <lucasoliveira226@gmail.com>
2021-10-13 17:24:47 -0300

    Requisito 3 completo

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,11 @@
 const data = require('../data/zoo_data');
 
+const { employees } = require('../data/zoo_data');
+
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+// seu código aqui
+  return employees.find(({ firstName, lastName }) =>
+    firstName === employeeName || lastName === employeeName) || {};
 }
 
 module.exports = getEmployeeByName;
