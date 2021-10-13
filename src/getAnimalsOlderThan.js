4ae7ca3491cf8a1830acc003bec2ae7f28bc240b
Lucas Oliveira <lucasoliveira226@gmail.com>
2021-10-13 16:16:30 -0300

    Correção de lint do requisito 1 e requisito 2 completo

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,14 @@
 const data = require('../data/zoo_data');
 
+const { species } = data;
+
 function getAnimalsOlderThan(animal, age) {
   // seu código aqui
+  const arrayFiltered = species.find((animalObj) => animalObj.name === animal).residents
+    .every((filteredAnimals) => filteredAnimals.age >= age);
+  return arrayFiltered;
 }
+
+console.log(getAnimalsOlderThan('lions', 3));
 
 module.exports = getAnimalsOlderThan;
--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -2,9 +2,9 @@ const data = require('../data/zoo_data');
 
 const { species } = data;
 
-const getSpeciesByIds = ((...ids) => {
+const getSpeciesByIds = ((...ids) =>
   // seu código aqui
-  return species.filter((idObject) => ids.includes(idObject.id));
-});
+  species.filter((idObject) => ids.includes(idObject.id))
+);
 
 module.exports = getSpeciesByIds;
