45283815811ee9e9b1b782857bfb2aa871fff882
Lucas Oliveira <lucasoliveira226@gmail.com>
2021-10-14 15:16:36 -0300

    Requisito 5 completo

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,23 @@
 const data = require('../data/zoo_data');
 
+const { species } = require('../data/zoo_data');
+
 function countAnimals(animal) {
   // seu código aqui
+  if (animal === undefined) {
+    const allAnimals = {};
+    species.forEach((specie) => {
+      allAnimals[specie.name] = specie.residents.length;
+    });
+    return allAnimals;
+  }
+  if (Object.keys(animal).length === 1) {
+    return species.find((specie) => specie.name === animal.specie).residents.length;
+  }
+
+  const animalAndSex = species.find((specie) => specie.name === animal.specie);
+
+  return animalAndSex.residents.filter((resident) => resident.sex === animal.sex).length;
 }
 
 module.exports = countAnimals;
